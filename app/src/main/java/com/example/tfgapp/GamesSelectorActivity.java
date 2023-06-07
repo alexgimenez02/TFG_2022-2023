723d0a33b0dd8574959de1ef22ff5be65b806751
Àlex Giménez <alex.gimenez02@estudiant.upf.edu>
2023-06-07 18:13:10 +0200

    chore: added button at game selector to launch game

--- app/src/main/java/com/example/tfgapp/GamesSelectorActivity.java
+++ app/src/main/java/com/example/tfgapp/GamesSelectorActivity.java
@@ -1,20 +1,33 @@
 package com.example.tfgapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.tfgapp.databinding.ActivityGamesSelectorBinding;
+import java.util.Objects;
 
 public class GamesSelectorActivity extends AppCompatActivity {
-
-    private ActivityGamesSelectorBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games_selector);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
+        Button pButton = findViewById(R.id.playButton);
+        pButton.setOnClickListener(view -> {
+           String packageName = "com.DefaultCompany.com.unity.template.mobile2D";
+            Intent intent = getPackageManager().getLaunchIntentForPackage(packageName);
+            if (intent != null) {
+                Toast.makeText(this, "Launching game!", Toast.LENGTH_SHORT);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "No Space Invaders Game installed", Toast.LENGTH_SHORT);
+            }
+        });
     }
 }
